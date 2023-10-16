e593b6eea04b0d0089ce615a14ca2ec09edb7f45
unknown <tobienabor@gmail.com>
2023-10-16 11:51:49 +0200

    added more comments to explain the code

--- 1/first_program.py
+++ 1/first_program.py
@@ -1,11 +1,17 @@
 fuel_price = 24.14
 
+# create a variable to store distance between jhb and durban
 durban_jhb_distance = 620
+# multiply the durban_jhb distance by 2 to get to and fro distance
 trip_distance = durban_jhb_distance * 2
 
+# create a variable to store the economy of vehicle
 car_economy = 0.5  # R/km
+
+# calculate the cost of trip by multiplying trip_distance by car economy
 cost_trip = trip_distance * car_economy
 
+# print to the terminal
 print(f"the cost of this trip was R{cost_trip}")
 
 print(2**2**3)
