497461baf0b527d11be9834c71dc2de506db9b14
unknown <tobienabor@gmail.com>
2023-10-16 11:40:23 +0200

    added the code for arithmetic operations with Python

---
+++ 1/first_program.py
@@ -0,0 +1,22 @@
+fuel_price = 24.14
+
+durban_jhb_distance = 620
+trip_distance = durban_jhb_distance * 2
+
+car_economy = 0.5  # R/km
+cost_trip = trip_distance * car_economy
+
+print(f"the cost of this trip was R{cost_trip}")
+
+print(2**2**3)
+"""
+right binding
+2 **3 = 8
+2** 8 = 256
+"""
+
+# division /
+print(6/3)
+print(6//3)
+print(-12//8)
+print(6%4)
---
+++ 1/python_fund_1.py
@@ -0,0 +1,43 @@
+# first program
+print("Hello world")
+
+# string literal
+"Hello, world"
+"Hello, world"
+
+# multiline string literal
+"""
+This is a python multiline string literal.
+"""
+string_multi = "hello!\
+    How are you doing?"
+
+print(string_multi)
+
+# numeric values
+123  # int
+-123
+5454546562889451  # int
+-54512358962  # also an int
+# floats
+
+123.76  # a weight in kgs - a float
+-123.432  # also a float
+
+# variables
+best_team = "Liverpool FC"
+print(best_team)
+
+# store my age in variable
+my_age = 52
+print(my_age)
+
+fuel_price = 24.16
+a_float = 24
+a_float2 = 3e5
+
+print(a_float2)
+
+# boolean literals - True and False
+a = 5 > 10
+print(a)  # False
