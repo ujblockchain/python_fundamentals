805ed6e23215d10e3bfa86eb3f6dbfe7d1db7fdb
unknown <tobienabor@gmail.com>
2023-10-17 19:38:06 +0200

    added code demonstrating how to add lists using list wrapper function

---
+++ 5_lists/lists_4.py
@@ -0,0 +1,18 @@
+# generating lists using range function
+# range generates a sequence from 0:
+# range(4) - 0 , 1 , 2 , 3
+# list() - a built in function that turns a sequence data type to a list
+my_list = list(range(4))
+print(my_list)
+
+# generate a list with for loop and range function
+my_list = [] # create an empty list
+
+for i in range(5):
+
+    my_list.insert(0, i)
+# this will create a reversed sequence of range(5)
+# range(5) - 0, 1, 2, 3, 4
+
+print(my_list) # 4, 3, 2, 1, 0
+
