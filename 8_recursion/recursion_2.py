0eba46a0dade008c1878ba25b2351b9a44c5d74f
unknown <tobienabor@gmail.com>
2023-10-30 11:32:46 +0200

    added advantages and disadvantages of recursions.

--- 8_recursion/recursion_2.py
+++ 8_recursion/recursion_2.py
@@ -8,6 +8,17 @@ Things to remember when dealing with a recursive function
 - Base case: final stop point or condition where recursive call needs to stop.
 it is used to prevent the occurrence of infinite loops. 
 
+
+Advantages of recursion
+
+1 - Used to create clean and elegant code
+2 - Complex tasks can be broken down and simplified using recursion
+3 - Sequence generation is simpler with recursion compared to nested iteration
+
+Disadvantages of recursion
+1 - Logic behind recursion can be hard to follow
+2 - Recursive calls are expensive and take up a lot of memory and time
+3 - Recursive functions can be difficult to debug
 """
 def recursion(count):
     # create the base case
